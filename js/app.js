9bda86b2b2d890e032ab0d05f211f8056e6ec1b2
alaa281997 <alaabbas97@gmail.com>
2021-04-07 13:17:36 +0300

    add strict

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-
+'use strict'
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
-let arrayHours = [`7 am `, `8 am `, `9 am `, `10 am `, `11 am `, `12 pm `,
+let arrayHours = [`6 am `,`7 am `, `8 am `, `9 am `, `10 am `, `11 am `, `12 pm `,
   `1 pm `, `2 pm `, `3 pm `, `4 pm `, `5 pm `, `6 pm `, `7 pm `];
 
 
